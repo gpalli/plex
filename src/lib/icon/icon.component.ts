7e5f4bb64abf17777fba2a3fa47ee6aff6770a23
Mariano Botta <marianoabotta@gmail.com>
2018-09-21 10:56:20 -0300

    agrega propiedad prefix a plex-icon
    Para elegir la webfont a utilizar.

--- src/lib/icon/icon.component.ts
+++ src/lib/icon/icon.component.ts
@@ -1,12 +1,12 @@
-import { NgForm } from '@angular/forms';
-import { Component, Input, HostBinding, HostListener, Optional, forwardRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'plex-icon',
-    template: `<i class="mdi mdi-{{name}}"></i> `,
+    template: `<i class="{{prefix}} {{prefix}}-{{name}}"></i> `,
 })
 export class PlexIconComponent {
     @Input() name: string;
+    @Input() prefix = 'mdi';
 
     constructor() {
 
